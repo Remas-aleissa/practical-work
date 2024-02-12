67c05644636ab2a6a8d6491cd407189e87cfee14
Remas-aleissa <157957211+Remas-aleissa@users.noreply.github.com>
2024-02-12 12:51:42 +0300

    added comment

--- coutin.cpp
+++ coutin.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 int main(){
-    string str;
-    cin >> str;
-    cout << str;
+  string str;
+   //cin >> str; cin will take the input from the user untill whitespace.  
+  getline(cin,str); //it will take the full line
+  cout << str;
 
     return 0;
 }
