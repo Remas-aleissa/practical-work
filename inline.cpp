928f55c75eb469c25a984da9fb1055f29c653320
Remas-aleissa <157957211+Remas-aleissa@users.noreply.github.com>
2024-02-12 13:06:37 +0300

    change number

--- inline.cpp
+++ inline.cpp
@@ -5,7 +5,7 @@ inline void printMe(string str){
     cout<< str << endl; //inline will replace printMe("Hi Remas") with thos code;
 }
 int main(){
-for(int i =0 ; i< 10000; i++){
+for(int i =0 ; i< 1000; i++){
     printMe("Hi Remas");
 }
 
