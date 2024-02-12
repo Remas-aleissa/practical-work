f7e290db9faae1141bbe2afaaa24c0acd27f58cb
Remas-aleissa <157957211+Remas-aleissa@users.noreply.github.com>
2024-02-12 13:07:01 +0300

    Update inline.cpp

--- inline.cpp
+++ inline.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 inline void printMe(string str){
-    cout<< str << endl; //inline will replace printMe("Hi Remas") with thos code;
+    cout<< str << endl; //inline will replace printMe("Hi Remas") with this code;
 }
 int main(){
 for(int i =0 ; i< 1000; i++){
